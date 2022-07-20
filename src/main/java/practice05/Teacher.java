549d7263cf4b60e2c38e67fbf4cdb69936d4d304
WUWI11 <wuwi11@oocl.com>
2022-07-20 16:39:07 +0800

    feat:finish practice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,29 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private int klass;
+
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass == 0) {
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach No Class.", this.name, this.age);
+        }else {
+            return String.format("My name is %s. I am %d years old. I am a Teacher. I teach Class %d.", this.name, this.age, this.klass);
+        }
+    }
 }
